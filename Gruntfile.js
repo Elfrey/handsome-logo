681b2ce5c6874a36a0f9d0332af3b9319c022ed7
Oganes Yayloyan <elfreyoganes@gmail.com>
2014-08-21 02:20:29 +0600

    -rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -425,7 +425,7 @@ module.exports = function (grunt) {
         'uglify',
         'copy:dist',
         'modernizr',
-        'rev',
+//        'rev',
         'usemin',
         'htmlmin'
     ]);
